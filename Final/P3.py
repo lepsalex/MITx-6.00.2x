a669a77090826c6f0e17ac1141d48cf836f57a04
Alex Lepsa <lepsalex@gmail.com>
2016-05-01 11:54:36 -0400

    Final: Problem 3A Complete

--- Final/P3.py
+++ Final/P3.py
@@ -6,6 +6,9 @@ MAXRABBITPOP = 1000
 CURRENTRABBITPOP = 500
 CURRENTFOXPOP = 30
 
+# debug variable
+debug = False
+
 def rabbitGrowth():
     """
     rabbitGrowth is called once at the beginning of each time step.
@@ -21,8 +24,22 @@ def rabbitGrowth():
     # you need this line for modifying global variables
     global CURRENTRABBITPOP
 
-    # TO DO
-    pass
+    # If current pop less than max pop ...
+    if (CURRENTRABBITPOP < MAXRABBITPOP):
+
+        # Calculate growth probability
+        threshold = 1.0 - (CURRENTRABBITPOP / float(MAXRABBITPOP))
+
+        # Get random value
+        prob = random.random()
+
+        # If random > threshold reproduce + 1 rabbit
+        if (prob > threshold):
+            CURRENTRABBITPOP += 1
+
+    if (debug == True):
+        print CURRENTRABBITPOP
+
 
 def foxGrowth():
     """
@@ -44,8 +61,29 @@ def foxGrowth():
     global CURRENTRABBITPOP
     global CURRENTFOXPOP
 
-    # TO DO
-    pass
+    # Fox reproduction and death thresholds
+    repThresh = (1 / float(3))
+    dieThresh = (1 / float(10))
+
+    # for each fox in the current fox pop
+    for fox in range(CURRENTFOXPOP):
+        # First determine if a fox eats rabbit
+        if (CURRENTRABBITPOP >= 10):
+            eatThresh = (CURRENTRABBITPOP / float(MAXRABBITPOP))
+            eatProb = random.random()
+            if ((eatProb < eatThresh) and (CURRENTRABBITPOP > 10)):
+                # Eat Rabbit
+                CURRENTRABBITPOP -= 1
+                # Attempt to Reproduce
+                repProb = random.random()
+                if (repProb < repThresh):
+                    CURRENTFOXPOP += 1
+            # Else if fox doesn't eat it may die ...
+            elif ((random.random() < dieThresh) and (CURRENTFOXPOP > 10)):
+                    CURRENTFOXPOP -= 1
+
+    if (debug == True):
+        print 'Rabbits: ' + str(CURRENTRABBITPOP) + ' - Foxes: ' + str(CURRENTFOXPOP)
 
 
 def runSimulation(numSteps):
@@ -60,5 +98,18 @@ def runSimulation(numSteps):
     Both lists should be `numSteps` items long.
     """
 
-    # TO DO
-    pass
+    # Lists of Populations to return
+    rabbit_populations = []
+    fox_populations = []
+
+    for x in range(numSteps):
+        # Run each function
+        rabbitGrowth()
+        foxGrowth()
+        # Append populations at end of timestep
+        rabbit_populations.append(CURRENTRABBITPOP)
+        fox_populations.append(CURRENTFOXPOP)
+
+    return (rabbit_populations, fox_populations)
+
+print runSimulation(200)
